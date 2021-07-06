1b90fe4c7133b60702d68446b2beba76a7c5459b
anshul <anshul.gupta36@gmail.com>
2021-07-07 02:09:43 +0530

    added library page content; added profile and logout button

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -6,18 +6,32 @@ import Dashboard from './pages/Dashboard';
 import Library from './pages/Library';
 import Explore from './pages/Explore';
 import Wallet from "./pages/Wallet";
+import profileIcon from "../src/assets/images/profile-icon.png";
+import logoutIcon from "../src/assets/images/logout-icon.png";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <header className="App-header"></header>
+        {/* <header className="App-header"></header> */}
         <Sidebar />
         <section className="main-section">
-          <Route path="/" component={Dashboard} exact />
-          <Route path="/library" component={Library} />
-          <Route path="/explore" component={Explore} />
-          <Route path="/wallet" component={Wallet} />
+          <div className="header flex">
+            <div className="profile-section flex items-center">
+              <img src={profileIcon} width="24px" alt="" />
+              <p>Praveen AX</p>
+            </div>
+            <div className="logout flex items-center">
+              <img src={logoutIcon} width="20px" alt="" />
+              <p>Logout</p>
+            </div>
+          </div>
+          <div className="pl-20">
+            <Route path="/" component={Dashboard} exact />
+            <Route path="/library" component={Library} />
+            <Route path="/explore" component={Explore} />
+            <Route path="/wallet" component={Wallet} />
+          </div>
         </section>
       </BrowserRouter>
     </div>
