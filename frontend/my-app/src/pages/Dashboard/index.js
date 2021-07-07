6c28e77c0a7d0450789fc7ba02be7b3fc162f48b
praveen kumar a x <xav.axp@gmail.com>
2021-07-07 17:47:16 +0530

    latest changes to server api

--- frontend/my-app/src/pages/Dashboard/index.js
+++ frontend/my-app/src/pages/Dashboard/index.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import './style.css';
+import ProgressBar from './progress';
+import {BsPlay} from 'react-icons/bs'
 
 const Dashboard = () => {
-    return <main className="dashboard-section">Dashboard</main>;
+
+    // const [progress,setProgess] = useState(0)
+
+    // useEffect(()=>{
+    //     //timer => update the progressbar -> 1,2,3
+    //     setInterval(()=>{
+    //         let tmp = progress +1
+    //         setProgess(tmp)
+    //         console.log(progress)
+    //     },1000)
+
+    // },[])
+
+
+    return <main className="dashboard-section">
+
+    <div  style={{width: '50%',margin: 'auto',display: 'block',position:'relative'}}>
+        <video id="videoPlayer" style={{width:'100%',top:'0px',bottom:"0px",left:"0px",right:"0px"}}  controls >
+            <source src="http://localhost:3000/video" type="video/mp4" />
+        </video>
+
+        <div style={{position:'absolute',width:'100%',top:'0px',bottom:"0px",left:"0px",right:"0px",background:'transparent'}}>
+            <BsPlay />
+            <div style={{position:'absolute',top:'15px',height:'5px',left:"2%",width:'30%',background:'grey'}}>
+                
+                <ProgressBar />
+                <span>Intro</span>
+            </div>
+            <div style={{position:'absolute',top:'15px',height:'5px',left:"34%",width:'30%',background:'grey'}}>
+            <ProgressBar />
+            </div>
+            <div style={{position:'absolute',top:'15px',height:'5px',left:"66%",width:'30%',background:'grey'}}>
+            <ProgressBar />
+            </div>
+        </div>
+    </div>
+    
+   
+    
+    </main>;
 }
 
 export default Dashboard;
