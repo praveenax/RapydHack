e9ee48d1e7cbded1be10ef816ce2f06d1a1475a4
anshul <anshul.gupta36@gmail.com>
2021-07-07 18:58:37 +0530

    remove sidebar and header from login page

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,22 +1,23 @@
 
 import './App.css';
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
 import Sidebar from './pages/Sidebar';
 import Dashboard from './pages/Dashboard';
 import Library from './pages/Library';
 import Explore from './pages/Explore';
 import Wallet from "./pages/Wallet";
-import profileIcon from "../src/assets/images/profile-icon.png";
-import logoutIcon from "../src/assets/images/logout-icon.png";
+import profileIcon from '../src/assets/images/profile-icon.png';
+import logoutIcon from '../src/assets/images/logout-icon.png';
 import Login from './pages/Login';
 
 function App() {
+  const location = useLocation();
+  console.log(location);
   return (
     <div className="App">
-      <BrowserRouter>
-        {/* <header className="App-header"></header> */}
-        <Sidebar />
-        <section className="main-section">
+      {location.pathname.includes("/login") ? null : <Sidebar />}
+      <section className="main-section">
+        {location.pathname.includes("/login") ? null : (
           <div className="header flex">
             <div className="profile-section flex items-center">
               <img src={profileIcon} width="24px" alt="" />
@@ -27,15 +28,15 @@ function App() {
               <p>Logout</p>
             </div>
           </div>
-          <div className="px-40">
-            <Route path="/" component={Dashboard} exact />
-            <Route path="/login" component={Login} />
-            <Route path="/library" component={Library} />
-            <Route path="/explore" component={Explore} />
-            <Route path="/wallet" component={Wallet} />
-          </div>
-        </section>
-      </BrowserRouter>
+        )}
+        <div className="px-40">
+          <Route path="/" component={Dashboard} exact />
+          <Route path="/login" component={Login} />
+          <Route path="/library" component={Library} />
+          <Route path="/explore" component={Explore} />
+          <Route path="/wallet" component={Wallet} />
+        </div>
+      </section>
     </div>
   );
 }
