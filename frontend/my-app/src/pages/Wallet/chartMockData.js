64d244284e4683816c95ea32e144c08ed5eef33a
anshul <anshul.gupta36@gmail.com>
2021-07-08 15:16:58 +0530

    added charts for wallet page

---
+++ frontend/my-app/src/pages/Wallet/chartMockData.js
@@ -0,0 +1,109 @@
+export const lineChartData = [
+  {
+    name: "Jan",
+    amt: 20,
+  },
+  {
+    name: "Feb",
+    amt: 10,
+  },
+  {
+    name: "Mar",
+    amt: 30,
+  },
+  {
+    name: "Apr",
+    amt: 45,
+  },
+  {
+    name: "May",
+    amt: 15,
+  },
+  {
+    name: "Jun",
+    amt: 90,
+  },
+  {
+    name: "Jul",
+    amt: 60,
+  },
+  {
+    name: "Aug",
+    amt: 5,
+  },
+  {
+    name: "Sep",
+    amt: 30,
+  },
+  {
+    name: "Oct",
+    amt: 80,
+  },
+  {
+    name: "Nov",
+    amt: 100,
+  },
+  {
+    name: "Dec",
+    amt: 10,
+  },
+];
+
+export const piedata = [
+  { name: "Group A", value: 60 },
+  { name: "Group B", value: 40 },
+  // { name: "Group C", value: 20 },
+  // { name: "Group D", value: 200 },
+];
+// const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+export const topicsWatchedData = [
+  {
+    name: "Jan",
+    watched: 2,
+  },
+  {
+    name: "Feb",
+    watched: 4,
+  },
+  {
+    name: "Mar",
+    watched: 6,
+  },
+  {
+    name: "Apr",
+    watched: 9,
+  },
+  {
+    name: "May",
+    watched: 3,
+  },
+  {
+    name: "Jun",
+    watched: 1,
+  },
+  {
+    name: "Jul",
+    watched: 4,
+  },
+  {
+    name: "Aug",
+    watched: 5,
+  },
+  {
+    name: "Sep",
+    watched: 7,
+  },
+  {
+    name: "Oct",
+    watched: 5,
+  },
+  {
+    name: "Nov",
+    watched: 10,
+  },
+  {
+    name: "Dec",
+    watched: 4,
+  },
+];
--- frontend/my-app/src/pages/Wallet/index.js
+++ frontend/my-app/src/pages/Wallet/index.js
@@ -4,85 +4,79 @@ import {
   Line,
   XAxis,
   YAxis,
-  // CartesianGrid,
   Tooltip,
   Legend,
-  // ResponsiveContainer,
+  PieChart,
+  Pie,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
 } from "recharts";
+import { lineChartData, piedata, topicsWatchedData } from "./chartMockData";
 import "./style.css";
 
-
-const lineChartData = [
-  {
-    name: "Jan",
-    amt: 20,
-  },
-  {
-    name: "Feb",
-    amt: 10,
-  },
-  {
-    name: "Mar",
-    amt: 30,
-  },
-  {
-    name: "Apr",
-    amt: 45,
-  },
-  {
-    name: "May",
-    amt: 15,
-  },
-  {
-    name: "Jun",
-    amt: 90,
-  },
-  {
-    name: "Jul",
-    amt: 60,
-  },
-  {
-    name: "Aug",
-    amt: 5,
-  },
-  {
-    name: "Sep",
-    amt: 30,
-  },
-  {
-    name: "Oct",
-    amt: 80,
-  },
-  {
-    name: "Nov",
-    amt: 100,
-  },
-  {
-    name: "Dec",
-    amt: 10,
-  },
-];
 
 const Wallet = () => {
   return (
     <div className="wallet-section">
       <h1>Wallet</h1>
-      <div className="w-full">
+      <div className="w-full mb-40 neumorphism">
         <h4>Money Spent Over Time </h4>
-        <LineChart
-          width={1000}
-          height={250}
-          data={lineChartData}
-          margin={{ top: 5, right: 0, left: 0, bottom: 5 }}
-        >
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="amt" stroke="#59D4E9" />
-        </LineChart>
+        <ResponsiveContainer width="100%" height={230}>
+          <LineChart
+            width={1000}
+            height={250}
+            data={lineChartData}
+            margin={{ top: 10, right: 10, left: -30, bottom: 5 }}
+          >
+            <XAxis dataKey="name" tickLine={false} />
+            <YAxis tickLine={false} interval={0} />
+            <Tooltip />
+            <Legend />
+            <Line type="linear" dataKey="amt" stroke="#59D4E9" />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
-      <div className=""></div>
+      <div className="flex">
+        <div className="time-spent-section neumorphism">
+          <h4>Time Spent on Video</h4>
+          <PieChart width={300} height={250}>
+            <Pie
+              data={piedata}
+              dataKey="value"
+              nameKey="name"
+              cx="150"
+              cy="50%"
+              innerRadius={60}
+              outerRadius={80}
+              fill="#82ca9d"
+              label
+            />
+          </PieChart>
+        </div>
+        <div className="watched-topics-section neumorphism">
+          <h4>Topics Watched</h4>
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              width={530}
+              height={300}
+              data={topicsWatchedData}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 5,
+                bottom: 5,
+              }}
+            >
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip wrapperClassName="custom-tooltip" />
+              <Legend />
+              <Bar dataKey="watched" fill="#8884d8" barSize={20} />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
     </div>
   );
 };
